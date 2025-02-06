5464b13e903f35382ecbf876843e494d45332537
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-06 12:13:25 +0800

    re structred

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var marks=prompt("Enter your marks : ");
 
-if(marks>=80)
+if (marks>100)
+    document.write("Invalide");
+
+else if(marks>=80 && marks<=100)
     document.write("A+");
-else if(marks>=70)
+else if(marks>=70 && marks<=79)
     document.write("A");
-else if(marks>=60)
+else if(marks>=60 && marks<=69)
     document.write("A-");
-else if(marks>=50)
+else if(marks>=50 && marks<=59)
     document.write("B+");
-else if(marks>=40)
+else if(marks>=40 && marks<=49)
     document.write("C");
 else 
     document.write("Fail");
